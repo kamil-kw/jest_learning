72e9dbece03c8b834a4c914e0015b15c45227365
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-12-07 17:25:41 +0000

    Adding code to just pass a test

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -2,9 +2,13 @@ const addition = require("../calc");
 
 describe("Calculator", () => {
     describe("Addition function", () => {
-        test("Should retourn 42 for 20 + 22", () => {
+        test("Should return 42 for 20 + 22", () => {
             expect(addition(20,22)).toBe(42);
-        })
+        });
+        test("Should return 73 for 42 + 31", () => {
+            expect(addition(42,31)).toBe(73);
+        });
+
     });
     describe("Substraction function", () => {
 
