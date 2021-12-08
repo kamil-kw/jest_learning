10c2d39d9727edfe6b25a9961e22aec19c13e3d0
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-12-08 18:10:58 +0000

    Mocks

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,12 +5,20 @@
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+    // document.body.innerHTML = "<p id='par'></p>";
 });
 
 describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
